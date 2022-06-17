2ec0668a5de573febc068573ea8312376302ebdd
Psalmsage <Olonijayesamson@gmail.com>
2022-06-17 19:17:41 +0100

    work on the Django models

---
+++ I4G0228411SD/blog/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    OlonijayeSamsonTobi = models.CharField(max_length=200)
+    Psalmsage = models.CharField(max_length=200)
+    Created_date = models.DateTimeField("17/6-6:18")
+    Published_date = models.DateTimeField("17/6-6:18")
